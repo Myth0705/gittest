a74cea957a5787b26aa1d9aa62df0c249cba5981
Myth <Myth@baofu.com>
2022-04-27 14:24:13 +0800

    first commit

---
+++ src/main/java/com/testgit/GitTest.java
@@ -0,0 +1,12 @@
+package com.testgit;
+
+/**
+ * @Author GoodMyth
+ * @Date 2022/4/27 14:12
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
