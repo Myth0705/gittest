fea920f6f40533f2a2501eafe68bbc0fd282ddf1
Myth <Myth@baofu.com>
2022-04-27 14:27:23 +0800

    second commit

--- src/main/java/com/testgit/GitTest.java
+++ src/main/java/com/testgit/GitTest.java
@@ -8,5 +8,6 @@ package com.testgit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
